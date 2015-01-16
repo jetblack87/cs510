2f3b3bcec860b8c63dfef6d7b0f4197e7d929758
mark <mwa29@drexel.edu>
2015-01-15 21:31:18 -0500

    Nightly commit... most functions are done... need to implement random walk and fix some issues.

--- src/mwa29/cs510/Main.java
+++ src/mwa29/cs510/Main.java
@@ -3,18 +3,29 @@
  */
 package mwa29.cs510;
 
+import java.io.File;
+
 /**
  * @author mark
  *
  */
 public class Main {
 
+	private final static String DEFAULT_FILE = "dat/SBP-level0.txt";
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length > 0) {
+			SlidingBrickProblem sbp = new SlidingBrickProblem(new File(args[0]));			
+		} else {
+			SlidingBrickProblem sbp = new SlidingBrickProblem(new File(DEFAULT_FILE));
 
+			SlidingBrickProblem.randomWalks(sbp.initialGameState, 100);
+			
+			
+		}
 	}
 
 }
