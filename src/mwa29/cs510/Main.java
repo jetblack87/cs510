d96b8c1c66999bfe3a6511d2777b7a915fcf7bd3
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2015-01-16 20:48:35 -0500

    Added documentation and argument parsing.

--- src/mwa29/cs510/Main.java
+++ src/mwa29/cs510/Main.java
@@ -1,29 +1,33 @@
 /**
- * 
+ * CS510
+ * Mark Albrecht
  */
 package mwa29.cs510;
 
 import java.io.File;
 
 /**
- * @author mark
- *
+ * The main entry into the program
  */
 public class Main {
 
-	private final static String DEFAULT_FILE = "dat/SBP-level3.txt";
-	
 	/**
 	 * @param args
+	 *            the command line argument to the application
 	 */
 	public static void main(String[] args) {
-		SlidingBrickProblem sbp = null;
-		if (args.length > 0) {
-			sbp = new SlidingBrickProblem(new File(args[0]));			
-		} else {
-			sbp = new SlidingBrickProblem(new File(DEFAULT_FILE));
+		for (int i = 0; i < args.length - 1; i++) {
+			if (args[i].equals("-file")) {
+				filePath = args[i + 1];
+			} else if (args[i].equals("-maxMoves")) {
+				maxMoves = Integer.parseInt(args[i + 1]);
+			}
 		}
-		
-		SlidingBrickProblem.randomWalks(sbp.initialGameState, 10000);
+
+		SlidingBrickProblem sbp = new SlidingBrickProblem(new File(filePath));
+		SlidingBrickProblem.randomWalks(sbp.initialGameState, maxMoves);
 	}
+
+	private static String filePath = "dat/SBP-level0.txt";
+	private static int maxMoves = 100;
 }
--- src/mwa29/cs510/SlidingBrickProblem.java
+++ src/mwa29/cs510/SlidingBrickProblem.java
@@ -1,3 +1,7 @@
+/**
+ * CS510
+ * Mark Albrecht
+ */
 package mwa29.cs510;
 
 import java.io.File;
@@ -10,36 +14,91 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * The SlidingBrickProblem class
+ */
 public class SlidingBrickProblem {
 
+	/**
+	 * Enumeration containing all of the directions a piece can move.
+	 */
 	public enum Direction {
 		up(0, -1), down(0, 1), left(-1, 0), right(1, 0);
 
+		/**
+		 * The delta for x when a piece moves in this direction
+		 */
 		private int dx;
+		/**
+		 * The delta for y when a piece moves in this direction
+		 */
 		private int dy;
 
+		/**
+		 * Constructs a direction with the given dx and dy
+		 * 
+		 * @param dx
+		 *            the delta for when this piece in moved
+		 * @param dy
+		 *            the delta for when this piece is moved
+		 */
 		Direction(int dx, int dy) {
 			this.dx = dx;
 			this.dy = dy;
 		}
 
+		/**
+		 * Gets dx
+		 * 
+		 * @return dx
+		 */
 		public int getDx() {
 			return dx;
 		}
 
+		/**
+		 * Gets dy
+		 * 
+		 * @return dy
+		 */
 		public int getDy() {
 			return dy;
 		}
 	}
 
+	/**
+	 * Class representing a move
+	 */
 	private static class GameMove {
 
+		/**
+		 * The game state before this move occurs
+		 */
 		private GameState beforeMove;
+		/**
+		 * The piece that is being moved
+		 */
 		private GamePiece pieceMoved;
+		/**
+		 * The direction that the piece is being moved
+		 */
 		private Direction moveDirection;
 
+		/**
+		 * The board after this move occurs
+		 */
 		private GameState afterMove;
 
+		/**
+		 * Constructs a GameMove with the given params
+		 * 
+		 * @param beforeMove
+		 *            the game state before this move
+		 * @param pieceMove
+		 *            the game piece that is being moved
+		 * @param moveDirection
+		 *            the direction that this piece is moving
+		 */
 		public GameMove(GameState beforeMove, GamePiece pieceMove,
 				Direction moveDirection) {
 			this.beforeMove = beforeMove;
@@ -78,6 +137,20 @@ public class SlidingBrickProblem {
 			}
 		}
 
+		/**
+		 * Helper method for applying the move
+		 * 
+		 * @param pieceMove
+		 *            the piece being moved
+		 * @param moveDirection
+		 *            the direction that the piece is moving
+		 * @param gameBoard
+		 *            the game board
+		 * @param y
+		 *            the x value
+		 * @param x
+		 *            the y value
+		 */
 		private void checkAndApplyMove(GamePiece pieceMove,
 				Direction moveDirection, final int[][] gameBoard, int y, int x) {
 			if (pieceMove.getPieceNumber() == gameBoard[x][y]) {
@@ -103,20 +176,50 @@ public class SlidingBrickProblem {
 			}
 		}
 
+		/**
+		 * Gets the {@link GameState} after this move occurs
+		 * 
+		 * @return the {@link GameState} after this move occurs
+		 */
 		public GameState getAfterMove() {
 			return afterMove;
 		}
 
+		/**
+		 * Gets the {@link GameState} before this move occurs
+		 * 
+		 * @return the {@link GameState} before this move occurs
+		 */
 		public GameState getBeforeMove() {
 			return beforeMove;
 		}
 
+		/**
+		 * Gets the move direction
+		 * 
+		 * @return the move direction
+		 */
 		public Direction getMoveDirection() {
 			return moveDirection;
 		}
 
+		/**
+		 * Gets the {@link GamePiece} that is being moved
+		 * 
+		 * @return the {@link GamePiece} that is being moved
+		 */
 		public GamePiece getPieceMoved() {
 			return pieceMoved;
+		}
+
+		/**
+		 * Gets a {@link String} that shows the move in a nice format
+		 * 
+		 * @return a {@link String} that shows the move in a nice format
+		 */
+		public String prettyToString() {
+			return String.format("(%d,%s)", pieceMoved.getPieceNumber(),
+					moveDirection.toString());
 		}
 
 		@Override
@@ -125,46 +228,94 @@ public class SlidingBrickProblem {
 					+ pieceMoved + ", moveDirection=" + moveDirection
 					+ ", afterMove=" + afterMove + "]";
 		}
-
-		public String prettyToString() {
-			return String.format("(%d,%s)", pieceMoved.getPieceNumber(),
-					moveDirection.toString());
-		}
 	}
 
+	/**
+	 * Class representing a game piece
+	 */
 	private static class GamePiece {
+		/**
+		 * Checks to see if this given piece is a game piece (and not empty or a
+		 * wall)
+		 * 
+		 * @param piece
+		 *            the piece to test
+		 * @return true if it's a game piece, false otherwise.
+		 */
+		public static boolean isGamePieceInt(int piece) {
+			return (EMPTY != piece && WALL != piece);
+		}
+
+		/**
+		 * The number of this piece
+		 */
 		private int pieceNumber;
 
+		/**
+		 * Constructs a {@link GamePiece} using the given piece number
+		 * 
+		 * @param pieceNumber
+		 *            the number of this piece
+		 */
 		public GamePiece(int pieceNumber) {
 			super();
 			this.pieceNumber = pieceNumber;
 		}
 
+		/**
+		 * Gets the piece number
+		 * 
+		 * @return the piece number
+		 */
 		public int getPieceNumber() {
 			return pieceNumber;
 		}
 
+		/**
+		 * Sets the piece number
+		 * 
+		 * @param pieceNumber
+		 *            the new piece number
+		 */
 		public void setPieceNumber(int pieceNumber) {
 			this.pieceNumber = pieceNumber;
 		}
-
-		public static boolean isGamePieceInt(int piece) {
-			return (EMPTY != piece && WALL != piece);
-		}
 	}
 
+	/**
+	 * Class representing the game state
+	 */
 	private static class GameState {
 
+		/**
+		 * The game board, duh!
+		 */
 		private int[][] gameBoard;
 
+		/**
+		 * The highest piece number - set during construction and used during
+		 * piece looping
+		 */
 		private int maxPiece = Integer.MIN_VALUE;
 
+		/**
+		 * Constructs a {@link GameState} using the given {@link File}
+		 * 
+		 * @param gameFile
+		 *            the file to load
+		 */
 		private GameState(File gameFile) {
 			loadGameState(gameFile);
 
 			setMaxPiece();
 		}
 
+		/**
+		 * Copy constructor
+		 * 
+		 * @param gameState
+		 *            the {@link GameState} to copy
+		 */
 		public GameState(GameState gameState) {
 			final int width = gameState.getWidth();
 			final int height = gameState.getHeight();
@@ -178,31 +329,44 @@ public class SlidingBrickProblem {
 			setMaxPiece();
 		}
 
-		public void setMaxPiece() {
-			final int width = this.getWidth();
-			final int height = this.getHeight();
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
-					if (GamePiece.isGamePieceInt(gameBoard[x][y])
-							&& gameBoard[x][y] > maxPiece) {
-						maxPiece = gameBoard[x][y];
-					}
-				}
-			}
-		}
-
+		/**
+		 * Applies the given {@link GameMove} to this {@link GameState}
+		 * 
+		 * @param gameMove
+		 *            the {@link GameMove} to apply
+		 */
 		public void applyMove(GameMove gameMove) {
 			gameBoard = gameMove.afterMove.gameBoard;
 		}
 
+		/**
+		 * Applies the given {@link GameMove} to a cloned {@link GameState} and
+		 * returns the result
+		 * 
+		 * @param gameMove
+		 *            the {@link GameMove} to apply
+		 * @return a clone of the resulting {@link GameState}
+		 */
 		public GameState applyMoveCloning(GameMove gameMove) {
 			return gameMove.afterMove.cloneGameState();
 		}
 
+		/**
+		 * Clones this {@link GameState}
+		 * 
+		 * @return a clone of this {@link GameState}
+		 */
 		public GameState cloneGameState() {
 			return new GameState(this);
 		}
 
+		/**
+		 * Compares this {@link GameState} against the given {@link GameState}
+		 * 
+		 * @param gameState
+		 *            the {@link GameState} to compare against
+		 * @return true if they're the same, false if they're not
+		 */
 		public boolean compare(GameState gameState) {
 			boolean equal = true;
 
@@ -237,6 +401,13 @@ public class SlidingBrickProblem {
 			return moveList;
 		}
 
+		/**
+		 * Returns all possible moves for the given {@link GamePiece}
+		 * 
+		 * @param gamePiece
+		 *            the {@link GamePiece} to move
+		 * @return all possible moves for the given {@link GamePiece}
+		 */
 		public List<GameMove> getAllMoves(GamePiece gamePiece) {
 			List<GameMove> moveList = new ArrayList<GameMove>();
 			for (Direction direction : Direction.values()) {
@@ -251,10 +422,20 @@ public class SlidingBrickProblem {
 			return moveList;
 		}
 
-		public int[][] getGameBoard() {
+		/**
+		 * Gets the game board representation
+		 * 
+		 * @return the game board representation
+		 */
+		private int[][] getGameBoard() {
 			return gameBoard;
 		}
 
+		/**
+		 * Gets the height of the game board
+		 * 
+		 * @return the height of the game board
+		 */
 		private int getHeight() {
 			if (null != gameBoard && gameBoard.length > 0) {
 				return gameBoard[0].length;
@@ -263,6 +444,11 @@ public class SlidingBrickProblem {
 			}
 		}
 
+		/**
+		 * Gets the width of the game board
+		 * 
+		 * @return the width of the game board
+		 */
 		private int getWidth() {
 			if (null != gameBoard) {
 				return gameBoard.length;
@@ -271,6 +457,11 @@ public class SlidingBrickProblem {
 			}
 		}
 
+		/**
+		 * Checks to see if this {@link GameState} is solved
+		 * 
+		 * @return true if this {@link GameState} is solved, false if not
+		 */
 		public boolean isSolved() {
 			final int height = this.getHeight();
 			final int width = this.getWidth();
@@ -284,6 +475,13 @@ public class SlidingBrickProblem {
 			return true;
 		}
 
+		/**
+		 * Loads the game state from the given {@link File}, updating the width,
+		 * height and gameBoard
+		 * 
+		 * @param gameFile
+		 *            the file to load
+		 */
 		private void loadGameState(File gameFile) {
 			try {
 				final Scanner fileScanner = new Scanner(gameFile);
@@ -321,6 +519,9 @@ public class SlidingBrickProblem {
 			}
 		}
 
+		/**
+		 * Normalizes this {@link GameState} Taken from homework 1 instructions
+		 */
 		public void normalize() {
 			int nextIdx = MASTER + 1;
 			final int height = this.getHeight();
@@ -337,21 +538,9 @@ public class SlidingBrickProblem {
 			}
 		}
 
-		private void swapIdx(int idx1, int idx2) {
-			final int height = this.getHeight();
-			final int width = this.getWidth();
-			for (int y = 0; y < height; y++) {
-				for (int x = 0; x < width; x++) {
-					if (gameBoard[x][y] == idx1) {
-						gameBoard[x][y] = idx2;
-					} else if (gameBoard[x][y] == idx2) {
-						gameBoard[x][y] = idx1;
-					}
-				}
-			}
-
-		}
-
+		/**
+		 * Prints the {@link GameState} to stdout
+		 */
 		public void outputGameState() {
 			if (null == gameBoard) {
 				throw new SlidingBrickProblemRuntimeException(
@@ -369,14 +558,67 @@ public class SlidingBrickProblem {
 				System.out.println("");
 			}
 		}
+
+		/**
+		 * Sets the maxPiece variable to the highest number on the board
+		 */
+		private void setMaxPiece() {
+			final int width = this.getWidth();
+			final int height = this.getHeight();
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					if (GamePiece.isGamePieceInt(gameBoard[x][y])
+							&& gameBoard[x][y] > maxPiece) {
+						maxPiece = gameBoard[x][y];
+					}
+				}
+			}
+		}
+
+		/**
+		 * Swaps the indexes - used to normalize {@link GameState}
+		 * 
+		 * @param idx1
+		 *            first index to swap
+		 * @param idx2
+		 *            second index to swap
+		 */
+		private void swapIdx(int idx1, int idx2) {
+			final int height = this.getHeight();
+			final int width = this.getWidth();
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					if (gameBoard[x][y] == idx1) {
+						gameBoard[x][y] = idx2;
+					} else if (gameBoard[x][y] == idx2) {
+						gameBoard[x][y] = idx1;
+					}
+				}
+			}
+
+		}
 	}
 
+	/**
+	 * Helper function to print messages when in DEBUG mode
+	 * 
+	 * @param message
+	 *            the message to print
+	 */
 	private static void debug(Object message) {
 		if (DEBUG) {
 			System.out.println("[DEBUG] " + message.toString());
 		}
 	}
 
+	/**
+	 * Tries to solve the puzzle by randomly executing available moves
+	 * 
+	 * @param gameState
+	 *            the {@link GameState} to walk
+	 * @param N
+	 *            the total number of turns to try
+	 */
 	public static void randomWalks(GameState gameState, int N) {
 		Random random = new Random();
 		boolean solved = false;
@@ -422,18 +664,42 @@ public class SlidingBrickProblem {
 
 	}
 
+	/**
+	 * The initial {@link GameState} for this puzzle
+	 */
 	GameState initialGameState;
 
+	/**
+	 * A 'goal' piece
+	 */
 	private static final int GOAL = -1;
 
+	/**
+	 * An 'empty' piece
+	 */
 	private static final int EMPTY = 0;
 
+	/**
+	 * A 'wall' piece
+	 */
 	private static final int WALL = 1;
 
+	/**
+	 * The 'master' brick
+	 */
 	private static final int MASTER = 2;
 
+	/**
+	 * True if we wanted to print debug statements
+	 */
 	private static final boolean DEBUG = false;
 
+	/**
+	 * Constructs this {@link SlidingBrickProblem} with the given {@link File}
+	 * 
+	 * @param gameFile
+	 *            the file to load
+	 */
 	public SlidingBrickProblem(File gameFile) {
 		initialGameState = new GameState(gameFile);
 	}
