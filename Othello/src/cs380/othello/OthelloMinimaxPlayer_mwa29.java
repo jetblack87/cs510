97d7ffb8baf3a7f11c976ee217095656e7d0ee8f
mark <mwa29@drexel.edu>
2015-02-26 20:36:07 -0500

    Switching over to a single function format (rather than one for min and one for max).

--- Othello/src/cs380/othello/OthelloMinimaxPlayer_mwa29.java
+++ Othello/src/cs380/othello/OthelloMinimaxPlayer_mwa29.java
@@ -17,20 +17,9 @@ public class OthelloMinimaxPlayer_mwa29 extends OthelloPlayer {
 	protected int maxDepth = INFINITE;
 
 	/**
-	 * The cache for storing playerone's generated moves
-	 */
-	private static Map<String, List<OthelloMove>> playeroneMoveCache;
-	/**
-	 * The cache for storing playertwo's generated moves
-	 */
-	private static Map<String, List<OthelloMove>> playertwoMoveCache;
-
-	/**
 	 * The default constructor
 	 */
 	public OthelloMinimaxPlayer_mwa29() {
-		playeroneMoveCache = new HashMap<String, List<OthelloMove>>();
-		playertwoMoveCache = new HashMap<String, List<OthelloMove>>();
 	}
 
 	/**
@@ -66,17 +55,14 @@ public class OthelloMinimaxPlayer_mwa29 extends OthelloPlayer {
 		OthelloMove returnMove = null;
 		int returnMoveScore = Integer.MIN_VALUE;
 
-		for (final OthelloMove a : generateMovesCache(state)) {
+		for (final OthelloMove a : state.generateMoves()) {
 			if (null == returnMove) {
 				returnMove = a;
 				returnMoveScore = Helper.playerScoreValue(
 						state.nextPlayerToMove,
 						state.applyMoveCloning(returnMove).score());
 			} else {
-				final int nextMoveScore = Helper.playerScoreValue(
-						state.nextPlayerToMove,
-						minValue(state.applyMoveCloning(a),
-								state.nextPlayerToMove, 0));
+				final int nextMoveScore = minimax(state, maxDepth, false);
 				if (nextMoveScore > returnMoveScore) {
 					returnMove = a;
 					returnMoveScore = nextMoveScore;
@@ -87,84 +73,27 @@ public class OthelloMinimaxPlayer_mwa29 extends OthelloPlayer {
 		return returnMove;
 	}
 
-	/**
-	 * The max-value portion of the minimax search
-	 * 
-	 * @param state
-	 *            The current state
-	 * @param currentPlayer
-	 *            The player that is making the move
-	 * @param currentDepth
-	 *            The current depth of the search
-	 * @return The maximized score of the min-values for all subsequent moves
-	 */
-	private int maxValue(final OthelloState state, final int currentPlayer,
-			final int currentDepth) {
-		if (state.gameOver()
-				|| (INFINITE != maxDepth && currentDepth > maxDepth)) {
+	private int minimax(OthelloState state, int currentDepth,
+			boolean maximizingPlayer) {
+		if (currentDepth == 0 || state.gameOver()) {
 			return state.score();
-		} else {
-			int v = Integer.MIN_VALUE;
-			for (final OthelloMove a : generateMovesCache(state)) {
-				v = Helper.Integer_max(v, Helper.playerScoreValue(
-						currentPlayer,
-						minValue(state.applyMoveCloning(a), currentPlayer,
-								currentDepth + 1)));
+		}
+		if (maximizingPlayer) {
+			int bestValue = Integer.MIN_VALUE;
+			for (final OthelloMove a : state.generateMoves()) {
+				int val = minimax(state.applyMoveCloning(a), currentDepth - 1,
+						false);
+				bestValue = Helper.Integer_max(bestValue, val);
 			}
-			return v;
-		}
-	}
-
-	/**
-	 * The min-value portion of the minimax search
-	 * 
-	 * @param state
-	 *            The current state
-	 * @param currentPlayer
-	 *            The player that is making the move
-	 * @param currentDepth
-	 *            The current depth of the search
-	 * @return The minimized score of the max-values for all subsequent moves
-	 */
-	private int minValue(final OthelloState state, final int currentPlayer,
-			final int currentDepth) {
-		if (state.gameOver()
-				|| (INFINITE != maxDepth && currentDepth > maxDepth)) {
-			return state.score();
+			return bestValue;
 		} else {
-			int v = Integer.MAX_VALUE;
-			for (final OthelloMove a : generateMovesCache(state)) {
-				v = Helper.Integer_min(v, Helper.playerScoreValue(
-						currentPlayer,
-						maxValue(state.applyMoveCloning(a), currentPlayer,
-								currentDepth + 1)));
+			int bestValue = Integer.MAX_VALUE;
+			for (final OthelloMove a : state.generateMoves()) {
+				int val = minimax(state.applyMoveCloning(a), currentDepth - 1,
+						true);
+				bestValue = Helper.Integer_min(bestValue, val);
 			}
-			return v;
+			return bestValue;
 		}
-	}
-
-	/**
-	 * A Cached version of generateMoves
-	 * 
-	 * Uses the playeroneMoveCache and playertwoMoveCache
-	 *
-	 * @param state
-	 *            the state used to generate the moves
-	 * @return the list of moves generated
-	 */
-	private List<OthelloMove> generateMovesCache(OthelloState state) {
-		final String stateString = state.toString();
-		List<OthelloMove> returnList;
-		Map<String, List<OthelloMove>> moveCache = OthelloState.PLAYER1 == state.nextPlayerToMove ? playeroneMoveCache
-				: playertwoMoveCache;
-
-		if (moveCache.containsKey(stateString)) {
-			returnList = moveCache.get(stateString);
-		} else {
-			returnList = state.generateMoves();
-			moveCache.put(stateString, returnList);
-		}
-
-		return returnList;
 	}
 }
