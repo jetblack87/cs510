c9e62b92aa68656e54420588bf3bee3ab81ce49d
mark <mwa29@drexel.edu>
2015-02-26 20:53:25 -0500

    Updated alphabeta to using single function rather than two.

--- Othello/src/cs380/othello/OthelloAlphaBetaPlayer_mwa29.java
+++ Othello/src/cs380/othello/OthelloAlphaBetaPlayer_mwa29.java
@@ -39,11 +39,9 @@ public class OthelloAlphaBetaPlayer_mwa29 extends OthelloMinimaxPlayer_mwa29 {
 						state.nextPlayerToMove,
 						state.applyMoveCloning(returnMove).score());
 			} else {
-				final int nextMoveScore = Helper.playerScoreValue(
-						state.nextPlayerToMove,
-						minValue(state.applyMoveCloning(a),
-								state.nextPlayerToMove, 0, returnMoveScore,
-								Integer.MAX_VALUE));
+				final int nextMoveScore = alphabeta(state, maxDepth,
+						Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+
 				if (nextMoveScore > returnMoveScore) {
 					returnMove = a;
 					returnMoveScore = nextMoveScore;
@@ -55,79 +53,51 @@ public class OthelloAlphaBetaPlayer_mwa29 extends OthelloMinimaxPlayer_mwa29 {
 	}
 
 	/**
-	 * The max-value portion of the minimax search
-	 * 
-	 * Performs alpha-beta pruning
+	 * Performs minimax using alpha-beta pruning
 	 * 
 	 * @param state
-	 *            The current state
-	 * @param currentPlayer
-	 *            The player that is making the move
+	 *            the current state
 	 * @param currentDepth
-	 *            The current depth of the search
-	 * @param currentMin
-	 *            The current minimum value found in the preceding minValue
-	 *            processing
-	 * @return The maximized score of the min-values for all subsequent moves
+	 *            the current depth
+	 * @param alpha
+	 *            the alpha variable
+	 * @param beta
+	 *            the beta variable
+	 * @param maximizingPlayer
+	 *            whether this is being called for the maximizing player
+	 * @return the heuristic score
 	 */
-	private int maxValue(final OthelloState state, final int currentPlayer,
-			final int currentDepth, int alpha, int beta) {
-		if (state.gameOver()
-				|| (INFINITE != maxDepth && currentDepth > maxDepth)) {
+	private int alphabeta(OthelloState state, int currentDepth, int alpha,
+			int beta, boolean maximizingPlayer) {
+		if (currentDepth == 0 || state.gameOver()) {
 			return state.score();
-		} else {
+		}
+		if (maximizingPlayer) {
 			int v = Integer.MIN_VALUE;
 			for (final OthelloMove a : state.generateMoves()) {
-				v = Helper.Integer_max(v, Helper.playerScoreValue(
-						currentPlayer,
-						minValue(state.applyMoveCloning(a), currentPlayer,
-								currentDepth + 1, alpha, beta)));
-
-				// Perform pruning
+				v = Helper.Integer_max(
+						v,
+						alphabeta(state.applyMoveCloning(a), currentDepth - 1,
+								alpha, beta, false));
 				alpha = Helper.Integer_max(alpha, v);
 				if (beta <= alpha) {
-					return v;
+					break;
 				}
 			}
 			return v;
-		}
-	}
-
-	/**
-	 * The min-value portion of the minimax search
-	 * 
-	 * Performs alpha-beta pruning
-	 * 
-	 * @param state
-	 *            The current state
-	 * @param currentPlayer
-	 *            The player that is making the move
-	 * @param currentDepth
-	 *            The current depth of the search
-	 * @param currentMax
-	 *            The current maximum value found in the preceding maxValue
-	 *            processing
-	 * @return The minimized score of the max-values for all subsequent moves
-	 */
-	private int minValue(final OthelloState state, final int currentPlayer,
-			final int currentDepth, int alpha, int beta) {
-		if (state.gameOver()
-				|| (INFINITE != maxDepth && currentDepth > maxDepth)) {
-			return state.score();
 		} else {
 			int v = Integer.MAX_VALUE;
 			for (final OthelloMove a : state.generateMoves()) {
-				v = Helper.Integer_min(v, Helper.playerScoreValue(
-						currentPlayer,
-						maxValue(state.applyMoveCloning(a), currentPlayer,
-								currentDepth + 1, alpha, beta)));
-
-				// Perform pruning
+				v = Helper.Integer_min(
+						v,
+						alphabeta(state.applyMoveCloning(a), currentDepth - 1,
+								alpha, beta, true));
 				beta = Helper.Integer_min(beta, v);
 				if (beta <= alpha) {
-					return v;
+					break;
 				}
 			}
+
 			return v;
 		}
 	}
